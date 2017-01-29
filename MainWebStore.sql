005f3eccafe8fea1aaa0e0ca97487dac25288d61
TheLaMarty <martinaalevin@gmail.com>
2017-01-29 15:18:08 +0100

    All tables

--- MainWebStore.sql
+++ MainWebStore.sql
@@ -1,12 +1,52 @@
 CREATE DATABASE Webstore;
 
+DROP DATABASE Webstore;
+
 USE Webstore;
 
-CREATE TABLE `Customer` (
-  `CustomerID` char(10) NOT NULL,
-  `FirstName` varchar(50) DEFAULT NULL,
-  `Surname` varchar(50) DEFAULT NULL,
-  `City` varchar(50) DEFAULT NULL
-)
-  
-  
+CREATE TABLE Customer (
+  CustomerID CHAR(10) PRIMARY KEY,
+  FirstName  VARCHAR(50) DEFAULT NULL,
+  Surname    VARCHAR(50) DEFAULT NULL,
+  City       VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE Order (
+  'OrderNumber' CHAR(20) PRIMARY KEY,
+  'SKU'         CHAR(15),
+  'Quantity'    TINYINT,
+  'OrderValue'  INT,
+  'Date'        DATE
+);
+
+CREATE TABLE Product (
+
+  SKU          CHAR(15) PRIMARY KEY,
+  Manufacturer VARCHAR(50),
+  Name         VARCHAR(50),
+  Size         VARCHAR(5),
+  Category     VARCHAR(50),
+  Price        CHAR(5)
+
+);
+
+CREATE TABLE Category (
+  CategoryID   CHAR(15) PRIMARY KEY,
+  CategoryName VARCHAR(50)
+);
+
+CREATE TABLE OutOfStock (
+
+  SKU  CHAR(15) PRIMARY KEY,
+  Date DATE
+
+);
+
+CREATE TABLE Inventory (
+
+  SKU          CHAR(15) PRIMARY KEY,
+  Manufacturer VARCHAR(50),
+  Stock        SMALLINT
+
+
+);
