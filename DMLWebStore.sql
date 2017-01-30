13a95fa1be90d9e0bc8171bc6727da073ab2e8ad
TheLaMarty <martinaalevin@gmail.com>
2017-01-30 16:37:41 +0100

    ......

--- DMLWebStore.sql
+++ DMLWebStore.sql
@@ -14,6 +14,25 @@ GROUP BY Category.CategoryName;
 
 -- #2 Skapa en kundlista med det totala ordervärdet kunden har beställt för. Lista kundens för- och efternamn, samt det totala ordervärdet.
 
+SELECT
+  CONCAT_WS(' ', Customer.FirstName, Customer.Surname),
+  SUM(Product.Price * OrderProducts.Quantity)
+FROM Customer
+  INNER JOIN `Order` ON Customer.CustomerID = Order.CustomerID
+  INNER JOIN OrderProducts ON Order.OrderID = OrderProducts.OrderID
+  INNER JOIN Product ON OrderProducts.SKU = Product.SKU
+GROUP BY ORDER.OrderID;
+
 -- #3 Vilka kunder har köpt blåa byxor  storlek 32 av märket Nudie?
 
+
 -- #4 Skriv ut en lista på det totala ordervärdet per ort
+
+SELECT
+  Customer.City,
+  SUM(Product.price * OrderProducts.Quantity)
+FROM Customer
+  INNER JOIN `Order` ON Customer.CustomerID = Order.CustomerID
+  INNER JOIN OrderProducts ON Order.OrderID = OrderProducts.OrderID
+  INNER JOIN Product ON OrderProducts.SKU = Product.SKU
+GROUP BY CUSTOMER.City;
