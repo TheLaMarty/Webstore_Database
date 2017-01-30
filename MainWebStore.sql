1e71f588bd7b28dc513306a265118c3dcd200108
TheLaMarty <martinaalevin@gmail.com>
2017-01-30 11:11:45 +0100

    Reformatted tables

--- MainWebStore.sql
+++ MainWebStore.sql
@@ -5,7 +5,7 @@ DROP DATABASE Webstore;
 USE Webstore;
 
 CREATE TABLE Customer (
-  CustomerID CHAR(10) PRIMARY KEY,
+  CustomerID INT NOT NULL PRIMARY KEY,
   FirstName  VARCHAR(50) DEFAULT NULL,
   Surname    VARCHAR(50) DEFAULT NULL,
   City       VARCHAR(50) DEFAULT NULL
@@ -13,32 +13,62 @@ CREATE TABLE Customer (
 
 CREATE TABLE Product (
 
-  SKU          CHAR(15) PRIMARY KEY,
+  SKU          INT NOT NULL PRIMARY KEY,
+  CategoryID   INT NOT NULL,
   Manufacturer VARCHAR(50),
   Name         VARCHAR(50),
   Size         VARCHAR(5),
   Category     VARCHAR(50),
-  Price        CHAR(5)
+  Price        DOUBLE
 
 );
 
 CREATE TABLE Category (
-  CategoryID   CHAR(15) PRIMARY KEY,
+  CategoryID   INT NOT NULL PRIMARY KEY,
   CategoryName VARCHAR(50)
 );
 
 CREATE TABLE OutOfStock (
 
-  SKU  CHAR(15) PRIMARY KEY,
+  SKU  INT NOT NULL PRIMARY KEY,
   Date DATE
 
 );
 
 CREATE TABLE Inventory (
 
-  SKU          CHAR(15) PRIMARY KEY,
+  SKU          INT NOT NULL PRIMARY KEY,
   Manufacturer VARCHAR(50),
   Stock        SMALLINT
 
+);
 
+CREATE TABLE ProductCategory (
+
+  CategoryID INT NOT NULL,
+  SKU        INT NOT NULL,
+
+  PRIMARY KEY (CategoryID, SKU)
+
+);
+
+CREATE TABLE OrderProducts (
+
+  OrderID INT NOT NULL,
+  SKU     INT NOT NULL,
+
+  PRIMARY KEY (OrderID, SKU)
+
+);
+
+
+CREATE TABLE `Order` (
+
+  OrderID    INT NOT NULL AUTO_INCREMENT,
+  CustomerID INT NOT NULL,
+  SKU        INT NOT NULL,
+  `Date`     DATE,
+  Quantity   TINYINT,
+  PRIMARY KEY (OrderID),
+  FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID)
 );
